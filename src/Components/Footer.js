ee421d185da1dd730affdff387f320f8d4e6bc1a
Niel <niel_ravelo@yahoo.com>
2024-08-02 10:49:37 -0700

    resrve auth

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () =>
     <>
       <footer>
         <div className="footer align-items-center ps-5 py-2">
-          <p>Cuisina - created by Nathaniel Ravelo</p>
+          <p>Cuisina - created by <a href="http://nathanielravelo.com/">Nathaniel Ravelo</a></p>
         </div>
       </footer>
     </>
--- src/Components/Reservation.js
+++ src/Components/Reservation.js
@@ -12,13 +12,7 @@ const Reservation = () =>
     time: ""
   })
 
-  const [errorMsg, setErrorMsg] = useState({
-    location: "",
-    email: "",
-    people: "",
-    date: "",
-    time: ""
-  })
+  const [isSubmitted, setIsSubmitted] = useState(true);
 
   const reserveInfo = (e) =>
   {
@@ -35,7 +29,12 @@ const Reservation = () =>
 
   const onSubmit = (data) =>
   {
-    console.log(data);
+    setIsSubmitted(false);
+  }
+
+  const newReservation = () =>
+  {
+    setIsSubmitted(true);
   }
 
   console.log(errors);
@@ -43,72 +42,133 @@ const Reservation = () =>
   return (
   <>
     <div className="bg-reserve">
-      <div className="container reserve-container d-flex justify-content-center">
-        <div className="reserve">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-3">
-                
-              {/* <label htmlFor="location" className="form-label">Location</label> */}
-                <select name="location" className="form-select reserve-form" onChange={(e) => reserveInfo(e)}>
-                <option>Select Location</option>
-                <option value="Pomona">Pomona</option>
-                <option value="Chino">Chino</option>
-                <option value="Glendora">Glendora</option>
-                <option value="Rancho Cucamonga">Rancho Cucamonga</option>
-              </select>
-            </div>
-            <div className="mb-3">
-                
-              {/* <label className="form-label" htmlFor="phone">Contact No.</label> */}
-              <input
-                className="form-control reserve-form"
-                type="email"
-                name="email"
-                placeholder="Email address"
-                onChange={(e) => reserveInfo(e)}
-              />
-            </div>
-            <div className="mb-3">
-                
-              {/* <label className="form-label" htmlFor="people">Table for</label> */}
-              <input 
-                className="form-control reserve-form" 
-                type="number" 
-                name="people"
-                min="1" 
-                max="12"
-                placeholder="Table for"
-                onChange={(e) => reserveInfo(e)}
-              />
-            </div>
-            <div className="mb-3">
-                
-              {/* <label className="form-label" htmlFor="date">Date</label> */}
-              <input 
-                className="form-control reserve-form" 
-                type="date" 
-                name="date" 
-                min={new Date().toISOString().split("T")[0]}
-                onChange={(e) => reserveInfo(e)}
-              />
-            </div>
-            <div className="mb-3">
-                
-              {/* <label htmlFor="time" className="form-label">Time</label> */}
-                <select name="time" className="form-select reserve-form" onChange={(e) => reserveInfo(e)}>
-                <option>Select Time</option>
-                {
-                  time.map((time) => (
-                    <option value={time.time}>{time.time}</option>
-                  ))
-                }
-              </select>
-            </div>
-            <button className="btn-red-small" type="submit">Reserve a Table</button>
-          </form>
+      {
+        isSubmitted ? 
+        <div className="container reserve-container d-flex justify-content-center">
+          <div className="reserve">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="mb-3">
+                <select name="location"
+                  className="form-select reserve-form"
+                  onChange={(e) => reserveInfo(e)}
+                  {...register("location", {
+                    required: true
+                  })}
+                >
+                  <option value="">Select Location</option>
+                  <option value="Pomona">Pomona</option>
+                  <option value="Chino">Chino</option>
+                  <option value="Glendora">Glendora</option>
+                  <option value="Rancho Cucamonga">Rancho Cucamonga</option>
+                </select>
+                {errors.location && errors.location.type === "required" && (
+                  <p className="errorMsg text-danger">Location is required.</p>
+                )}
+              </div>
+              <div className="mb-3">
+
+                {/* <label className="form-label" htmlFor="phone">Contact No.</label> */}
+                <input
+                  className="form-control reserve-form"
+                  type="text"
+                  name="email"
+                  placeholder="Email address"
+                  onChange={(e) => reserveInfo(e)}
+                  {...register("email", {
+                    required: true,
+                    pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/
+                  })}
+                />
+                {errors.email && errors.email.type === "required" && (
+                  <p className="errorMsg text-danger">Email is required.</p>
+                )}
+                {errors.email && errors.email.type === "pattern" && (
+                  <p className="errorMsg text-danger">Email is not valid.</p>
+                )}
+              </div>
+              <div className="mb-3">
+
+                {/* <label className="form-label" htmlFor="people">Table for</label> */}
+                <input
+                  className="form-control reserve-form"
+                  type="number"
+                  name="people"
+                  placeholder="Table for"
+                  onChange={(e) => reserveInfo(e)}
+                  {...register("people", {
+                    required: true,
+                    valueAsNumber: true,
+                    max: 12,
+                    min: 1
+                  })}
+                />
+                {errors.people && errors.people.type === "required" && (
+                  <p className="errorMsg text-danger">Number of guests is required.</p>
+                )}
+                {errors.people && errors.people.type === "min" && (
+                  <p className="errorMsg text-danger">Minimum number of guests is 1</p>
+                )}
+                {errors.people && errors.people.type === "max" && (
+                  <p className="errorMsg text-danger">Maximum number of guests is 12</p>
+                )}
+              </div>
+              <div className="mb-3">
+
+                {/* <label className="form-label" htmlFor="date">Date</label> */}
+                <input
+                  className="form-control reserve-form"
+                  type="date"
+                  name="date"
+                  min={new Date(new Date().setDate(new Date().getDate() + 2)).toISOString().split("T")[0]}
+                  onChange={(e) => reserveInfo(e)}
+                  {...register("date", {
+                    required: true
+                  })}
+                />
+                {errors.date && errors.date.type === "required" && (
+                  <p className="errorMsg text-danger">Date is required.</p>
+                )}
+              </div>
+              <div className="mb-3">
+
+                {/* <label htmlFor="time" className="form-label">Time</label> */}
+                <select
+                  name="time"
+                  className="form-select reserve-form"
+                  onChange={(e) => reserveInfo(e)}
+                  {...register("time", {
+                    required: true
+                  })}
+                >
+                  <option value="">Select Time</option>
+                  {
+                    time.map((time) => (
+                      <option value={time.time}>{time.time}</option>
+                    ))
+                  }
+                </select>
+                {errors.time && errors.time.type === "required" && (
+                  <p className="errorMsg text-danger">Time is required.</p>
+                )}
+              </div>
+              <button className="btn-red-small" type="submit">Reserve a Table</button>
+            </form>
+          </div>
         </div>
-      </div>
-      </div>
+
+        :
+        
+        <div className="container reserve-container-complete d-flex justify-content-center">
+          <div className="reserve text-white">
+            <h3>Your reservation has been confirmed</h3>
+                <h3>Click <span className="text-primary ptr" onClick={newReservation}>here</span> to make another reservation</h3>
+          </div>
+          
+        </div>
+        
+      }
+      
+    </div>
   </>
   )
 }
